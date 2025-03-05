e494c0e7336afd335f0647f202ad7e07204d5fe1
Temirlan Tlektessov <ttlektessov@gmail.com>
2025-03-05 13:23:27 +0500

    added link to figma

--- src/components/General/ConstructionContainer.jsx
+++ src/components/General/ConstructionContainer.jsx
@@ -6,6 +6,13 @@ export default function ContstructionContainer() {
       <hr className="my-8 h-px w-full border-0 bg-gray-200"></hr>
       <Construction color="#6b7280" />
       <p className="h1">Under construction</p>
+      <a
+        href="https://www.figma.com/design/U7ccWBPLozqfdhxmXelDNe/Tlektessov-Temirlan-Portfolio?node-id=327-868&t=nsxElfABYr4vupQS-1"
+        target="_blank"
+        className="h3 mb-8"
+      >
+        Figma design
+      </a>
     </div>
   );
 }
