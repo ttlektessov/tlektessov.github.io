b4cfa31f61b4cf0b9304f08af21077e028caeec0
Temirlan Tlektessov <ttlektessov@gmail.com>
2025-03-12 21:22:41 +0500

    added memo to optimize icon rendering

--- src/components/General/IconButton.jsx
+++ src/components/General/IconButton.jsx
@@ -1,38 +1,40 @@
-import { forwardRef } from "react";
+import { forwardRef, memo } from "react";
 // add memo to prevent re-render
-const IconButton = forwardRef(
-  (
-    {
-      className = "",
-      size = "md",
-      showTooltip = false,
-      tooltipText = "",
-      children,
-      ...props
+const IconButton = memo(
+  forwardRef(
+    (
+      {
+        className = "",
+        size = "md",
+        showTooltip = false,
+        tooltipText = "",
+        children,
+        ...props
+      },
+      ref,
+    ) => {
+      const baseClasses =
+        "hover:cursor-pointer flex justify-center items-center hover:bg-gray-100 active:bg-gray-200 rounded-lg p-1.5 transition-colors duration-200";
+      const iconClasses =
+        "[&_svg]:stroke-gray-600 [&_svg]:hover:stroke-gray-700";
+      const sizeClasses = {
+        md: "[&_svg]:w-6 [&_svg]:h-6",
+        lg: "[&_svg]:w-8 [&_svg]:h-8",
+      };
+
+      const buttonClasses = `relative ${baseClasses} ${iconClasses} ${sizeClasses[size]} ${className}`;
+      return (
+        <button className={buttonClasses} ref={ref} {...props}>
+          {children}
+          {showTooltip && tooltipText.length > 0 && (
+            <span className="body3 absolute -top-8 rounded-lg bg-gray-200 px-2 py-1 text-sm">
+              {tooltipText}
+            </span>
+          )}
+        </button>
+      );
     },
-    ref,
-  ) => {
-    const baseClasses =
-      "hover:cursor-pointer flex justify-center items-center hover:bg-gray-100 active:bg-gray-200 rounded-lg p-1.5 transition-colors duration-200";
-    const iconClasses = "[&_svg]:stroke-gray-600 [&_svg]:hover:stroke-gray-700";
-    const sizeClasses = {
-      md: "[&_svg]:w-6 [&_svg]:h-6",
-      lg: "[&_svg]:w-8 [&_svg]:h-8",
-    };
-
-    const buttonClasses = `relative ${baseClasses} ${iconClasses} ${sizeClasses[size]} ${className}`;
-
-    return (
-      <button className={buttonClasses} ref={ref} {...props}>
-        {children}
-        {showTooltip && tooltipText.length > 0 && (
-          <span className="body3 absolute -top-8 rounded-lg bg-gray-200 px-2 py-1 text-sm">
-            {tooltipText}
-          </span>
-        )}
-      </button>
-    );
-  },
+  ),
 );
 
 IconButton.displayName = "IconButton";
